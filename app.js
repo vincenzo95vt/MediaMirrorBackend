eeb25fb42341c066df1ad1e58f0324fa08b6b4d7
Vinnie <pablovasta1995@gmail.com>
2025-03-09 16:24:28 +0100

    add

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require("express")
 const app = express()
-const cors = require("cors")
+const cors = require("cors");
+const { scrapeNews } = require(".");
 app.use(express.json());
 app.use(cors());
 
 
 const port = 3000
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.post("/scrape", async (req, res) => {
+    const url = req.body.url;
+    
+    if(!url) {
+        return res.status(400).json({error: "Se requiere una url"})
+    }
+
+    const content = await scrapeNews(url);
+    if(!content) {
+        return res.status(400).json({error: "No se pudo obtener el contenido de la noticia"});
+    }
+})
