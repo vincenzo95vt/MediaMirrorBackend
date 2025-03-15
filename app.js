20f02a7151e2b21ecad1af61cd91774c55d74a2b
Vinnie <pablovasta1995@gmail.com>
2025-03-15 02:06:12 +0100

    deleting chromium

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 const puppeteer = require("puppeteer");
-const chromium = require("chrome-aws-lambda");
-
-
 
 const { getContent } = require("./content");
 const { urlCheck } = require("./utils");
@@ -22,14 +19,8 @@ const scrapeNews = async (url) => {
                 message: "Ups, parece que tu periódico no es de confianza."
             };
         }else{
-            const browser = await chromium.puppeteer.launch({
-                args: chromium.args,
-                defaultViewport: chromium.defaultViewport,
-                executablePath: await chromium.executablePath,
-                headless: chromium.headless
-            });
+            const browser = await puppeteer.launch({ headless: true });
             const page = await browser.newPage();
-            await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36");
             await page.goto(url, { waitUntil: "domcontentloaded" });
         
             const content = await page.evaluate(() => document.body.innerText);
@@ -53,7 +44,6 @@ console.log("OPENAI_API", process.env.OPENAI_API_KEY)
 
 const returnNews = async (url) => {
     try {
-        
         const scrapedContent = await scrapeNews(url);
         console.log(scrapedContent)
         if(scrapedContent.error){
