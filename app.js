2499d5bce9d492b8a28248e917b43dcfbeaa13b7
Vinnie <pablovasta1995@gmail.com>
2025-03-15 01:43:14 +0100

    adding chromium

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const puppeteer = require("puppeteer");
+const chromium = require("chrome-aws-lambda");
+
+const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless
+});
+
 const { getContent } = require("./content");
 const { urlCheck } = require("./utils");
 
@@ -20,6 +29,7 @@ const scrapeNews = async (url) => {
         }else{
             const browser = await puppeteer.launch({ headless: true });
             const page = await browser.newPage();
+            await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36");
             await page.goto(url, { waitUntil: "domcontentloaded" });
         
             const content = await page.evaluate(() => document.body.innerText);
