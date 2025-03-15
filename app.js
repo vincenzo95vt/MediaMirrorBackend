a47b7708e27708caae120af885f34014b1b4b2cf
Vinnie <pablovasta1995@gmail.com>
2025-03-15 02:14:47 +0100

    force puppeteer in render

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const scrapeNews = async (url) => {
             const browser = await puppeteer.launch({
                 args: ["--no-sandbox", "--disable-setuid-sandbox"],
                 headless: "new",
-                executablePath: puppeteer.executablePath() 
+                executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || puppeteer.executablePath() 
             });
             const page = await browser.newPage();
             await page.goto(url, { waitUntil: "domcontentloaded" });
